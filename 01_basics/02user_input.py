0f5f32090b39b3176e2f0edcef82983a2fc65947
seraapb <119811897+seraapb@users.noreply.github.com>
2024-04-26 11:28:51 +0200

    Initial commit

---
+++ 01_basics/02user_input.py
@@ -0,0 +1,29 @@
+"""
+Input refers to any information a user gives to the program.
+Specifically, the Python command input reads in a line of input typed in by the user.
+It may also be used to display a message to the user, to prompt for specific input.
+
+The following program reads in the name of the user with the input command.
+It then prints it out with the print command:
+"""
+# name = input("What is your name? ")
+# print("Hi there, " + name)
+
+"""
+Variables
+The word 'name' in this program is a variable. 
+In the context of programming, a variable is a location for storing some value, such as a string or a number. 
+This value can be used later, and it can also be changed.
+"""
+
+"""
+Referencing variables
+A single variable can be referred to many times in a program:
+"""
+# name = input("What is your name? ")
+#
+# print("Hi, " + name + "!")
+# print(name + " is quite a nice name.")
+
+
+
---
+++ 01_basics/exercises/01printing.py
@@ -0,0 +1,33 @@
+"""
+Write a program which prints out the following lines exactly as they are written:
+
+Row, row, row your boat,
+Gently down the stream.
+Merrily, merrily, merrily, merrily,
+Life is but a dream.
+"""
+# Write your solution here
+
+"""
+Print the multiplication of 2*2 using a combination of string and operation
+The program should print "the result = 4"
+"""
+# Write your solution here
+
+"""
+Write a program which prints out: print("Hello there!")
+"""
+# Write your solution here
+
+"""
+Fix this program so that the entire calculation, complete with result, is printed out on a single line. 
+Do not change the number of print commands used.
+"""
+# Fix the code
+print(5)
+print(" + ")
+print(8)
+print(" - ")
+print(4)
+print(" = ")
+print(5 + 8 - 4)
